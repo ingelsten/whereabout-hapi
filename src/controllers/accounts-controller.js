2a1c3e8c49a71d271e4dd15c45ec67b87aa567eb
Anders <77015471+ingelsten@users.noreply.github.com>
2022-05-18 07:19:31 +0100

    bcrypt update
    bcrypt update

--- src/controllers/accounts-controller.js
+++ src/controllers/accounts-controller.js
@@ -1,22 +1,36 @@
+import bcrypt from "bcrypt";          // ADDED
+
+import { UserSpec, UserCredentialsSpec } from "../models/joi-schemas.js";
 import { db } from "../models/db.js";
+
+
+const saltRounds = 10;                // ADDED
 
 export const accountsController = {
   index: {
     auth: false,
     handler: function (request, h) {
-      return h.view("Main", { title: "Welcome to Playlist" });
+      return h.view("main", { title: "Welcome to Whereabout" });
     },
   },
   showSignup: {
     auth: false,
     handler: function (request, h) {
-      return h.view("Signup", { title: "Sign up for Playlist" });
+      return h.view("Signup", { title: "Sign up for Whereabout" });
     },
   },
   signup: {
     auth: false,
+    validate: {
+      payload: UserSpec,
+      options: { abortEarly: false },
+      failAction: function (request, h, error) {
+        return h.view("Signup", { title: "Sign up error", errors: error.details }).takeover().code(400);
+      },
+    },
     handler: async function (request, h) {
       const user = request.payload;
+      user.password = await bcrypt.hash(user.password, saltRounds);    // ADDED
       await db.userStore.addUser(user);
       return h.redirect("/");
     },
@@ -24,15 +38,23 @@ export const accountsController = {
   showLogin: {
     auth: false,
     handler: function (request, h) {
-      return h.view("Login", { title: "Login to Playlist" });
+      return h.view("Login", { title: "Login to Whereabout" });
     },
   },
   login: {
     auth: false,
+    validate: {
+      payload: UserCredentialsSpec,
+      options: { abortEarly: false },
+      failAction: function (request, h, error) {
+        return h.view("Login", { title: "Log in error", errors: error.details }).takeover().code(400);
+      },
+    },
     handler: async function (request, h) {
       const { email, password } = request.payload;
       const user = await db.userStore.getUserByEmail(email);
-      if (!user || user.password !== password) {
+      const passwordsMatch = await bcrypt.compare(password, user.password);    // ADDED
+      if (!user || !passwordsMatch) {                                          // EDITED
         return h.redirect("/");
       }
       request.cookieAuth.set({ id: user._id });
